1f896468cf35565bf2b81edfa4f86613a056fa84
Urmil Vora <urmilvora910@gmail.com>
2021-04-19 22:01:35 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,26 @@
+#creating majority - minority dataset
+import pandas
+import matplotlib.pyplot as plt
+datainput = pandas.read_csv('creditcard.csv')
+minority = datainput[datainput['Class'] == 1]
+majority = datainput[datainput['Class'] == 0]
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+str1='Majority Dataset('+str(len(majority))+')'
+str2='Minority Dataset('+str(len(minority))+')'
+fieldName = [str1, str2]
+fieldSize = [len(majority),len(minority)]
+ax.bar(fieldName,fieldSize,color=['deepskyblue','lime'])
+plt.show()
+#plotting majority and minority datasets
+plt.plot(majority["V1"], majority["V2"], "o", color="deepskyblue")
+plt.plot(minority["V1"], minority["V2"], "o", color="lime")
+plt.show()
+#plotting minority datasets as k means clusters
+from sklearn.cluster import KMeans
+kmeans = KMeans(2)
+kmeans.fit(minority)
+clustors = minority.copy()
+clustors['cluster_pred'] = kmeans.fit_predict(minority)
+plt.scatter(clustors['V1'], clustors['V2'], c=clustors['cluster_pred'], cmap='autumn')
+plt.show()
